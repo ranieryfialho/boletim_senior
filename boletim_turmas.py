92ae43398dd5b733922282dbd4a6593aeae83e5b
Raniery Fialho <ranieryfialho@gmail.com>
2024-04-27 12:10:17 +0000

    update files

--- boletim_turmas.py
+++ boletim_turmas.py
@@ -7,4 +7,5 @@ st.subheader("Escolha a turma")
 turmas = st.selectbox("Turmas", ["2001-A","2002-A","2003-A","2004-A"])
 # st.write(turmas)]
 
-boletim = pd.read_csv('')
+boletim = pd.read_csv('https://raw.githubusercontent.com/ranieryfialho/boletim_senior/main/Boletim%20de%20Todas%20as%20turmas.csv')
+st.write(boletim)
