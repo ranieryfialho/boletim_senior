ab99ec2ab1b8e2f58ed0228e60f795741a45813d
Raniery Fialho <ranieryfialho@gmail.com>
2024-04-28 14:32:02 +0000

    update

--- boletim_turmas.py
+++ boletim_turmas.py
@@ -4,24 +4,12 @@ import pandas as pd
 st.title("Boletim Sênior Profissões")
 st.subheader("Escolha a turma")
 
-turmas = st.selectbox("Turmas", ["2001-A","2002-A","2003-A","2004-A"])
-botao = st.button("Filtrar")
-# st.write(botao)
-
 boletim = pd.read_csv('https://raw.githubusercontent.com/ranieryfialho/boletim_senior/main/BOLETIM%20beta.csv')
 st.dataframe(boletim)
+
+filtrar = st.selectbox("Turmas", boletim["TURMA"].unique())
+botao = st.button("Filtrar")
 st.write("-"*80)
 
-turmas_selecao = boletim["turmas"].unique()
-
-# df = pd.read_csv("https://raw.githubusercontent.com/hermeson883/data_science_workshop/main/linguagens.csv")
-
-# st.dataframe(df)
-
-# linguagens = df["Linguagem"].unique()
-
-# escolha = st.selectbox("Escolha um", linguagens)
-
-# if st.button("Click em mim"):
-#     filtro = df.loc[df["Linguagem"] == escolha]
-#     st.write(filtro)
+if botao == True:
+    boletim.loc[boletim["TURMA"] == "filtrar"]
