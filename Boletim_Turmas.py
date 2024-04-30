2865839f8f08cd2cc038e11eaea7fd00897b625b
Raniery Fialho <ranieryfialho@gmail.com>
2024-04-30 18:26:05 +0000

    adicionado grafico ao filtro por aluno. adicionado requiments.txt

--- Boletim_Turmas.py
+++ Boletim_Turmas.py
@@ -2,7 +2,9 @@ import streamlit as st
 
 st.markdown("<h1 style='text-align: center;'>Boletim - Sênior Profissões</h1>", unsafe_allow_html=True)
 st.markdown("""
-    <p style='text-align: justify;'>A aplicação é destinada a facilitar a visualização e o gerenciamento de dados de alunos de uma instituição de ensino, apresentados em um boletim. Utilizando uma interface interativa, a aplicação permite aos usuários visualizar um boletim completo e aplicar filtros específicos por turma ou por nome do aluno. Os filtros podem ser ajustados para procurar por turmas específicas através de um menu dropdown ou por partes do nome do aluno, oferecendo flexibilidade na busca e melhorando a usabilidade para o usuário final. Isso torna a ferramenta extremamente útil para administradores escolares, professores ou até mesmo alunos que buscam informações específicas dentro de um conjunto de dados educacionais.</p>
+            <p style='text-align: justify;'>A aplicação é destinada a facilitar a visualização e o gerenciamento de dados de alunos da Sênior Profissões, apresentados em um boletim. 
+            Utilizando uma interface interativa, a aplicação permite aos usuários visualizar um boletim completo e aplicar filtros específicos por turma ou por nome do aluno. Os filtros podem ser ajustados para procurar por turmas específicas através de um menu dropdown ou por partes do nome do aluno, oferecendo flexibilidade na busca e melhorando a usabilidade para o usuário final. 
+            Isso torna a ferramenta extremamente útil para administradores escolares, professores ou até mesmo alunos que buscam informações específicas dentro de um conjunto de dados educacionais.</p>
 """, unsafe_allow_html=True)
 
 link = "https://ranieryfialho.github.io/linktree/"
--- pages/Filtro_Aluno.py
+++ pages/Filtro_Aluno.py
@@ -5,11 +5,22 @@ st.title("Filtro por Aluno")
 st.subheader("Escolha a turma")
 
 boletim = pd.read_csv('https://raw.githubusercontent.com/ranieryfialho/boletim_senior/main/BOLETIM%20beta.csv')
-# st.dataframe(boletim)
 
-filtar_aluno = st.text_input("Digite o nome do aluno: ").strip().lower()
-botao_aluno = st.button("Filtrar por Aluno")
+if "boletim_filtrado_aluno" not in st.session_state:
+    st.session_state.boletim_filtrado_aluno = pd.DataFrame()
+
+filtrar_aluno = st.text_input("Digite o nome do aluno: ").strip().lower()
+botao_aluno = st.button("Filtrar")
 
 if botao_aluno:
-    boletim_filtrado_aluno = boletim.loc[boletim["ALUNOS"].str.lower().str.contains(filtar_aluno)]
-    st.dataframe(boletim_filtrado_aluno)
+    st.session_state.boletim_filtrado_aluno = boletim[boletim["ALUNOS"].str.lower().str.contains(filtrar_aluno)]
+    st.dataframe(st.session_state.boletim_filtrado_aluno) 
+
+if not st.session_state.boletim_filtrado_aluno.empty:
+    aluno_selecionado = st.selectbox("Selecione um aluno para ver as notas", st.session_state.boletim_filtrado_aluno['ALUNOS'].unique())
+    botao_notas = st.button('Mostrar Gráfico de Notas')
+    
+    if botao_notas:
+        notas_aluno = boletim.loc[boletim['ALUNOS'].str.lower() == aluno_selecionado.lower(), ['IRS', 'CRD', 'CTF', 'APG', 'PTS', 'ILT',
+                                                                                            'ADM', 'DP', 'SCT', 'CONT', 'TCC']]
+        st.bar_chart(notas_aluno.T) 
